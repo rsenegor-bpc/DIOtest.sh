37e1cadf0b711a6cda8630806b04f6472307b2d5
rsenegor-bpc <44207137+rsenegor-bpc@users.noreply.github.com>
2018-10-23 13:58:23 -0700

    Update DIOtest.sh

--- DIOtest.sh
+++ DIOtest.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+echo "Installing updates..."
+sudo apt-get update
 
-cd ..
-cd Desktop
+sudo apt-get install -y git-all
+
+sudo adduser mtsnlinux --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+echo password | sudo chpasswd
+
+echo "Installing DIO test software..."
+sudo git clone https://github.com/rsenegor-bpc/finalDIO.git /home/mtsnlinux/Desktop
+sudo git clone https://github.com/rsenegor-bpc/DIOtest.sh.git /home/mtsnlinux/DIO
+
+cd /home/mtsnlinux/Desktop
 cd powerdaq-3.6.26
 
 echo "Installing drivers..."
@@ -9,11 +19,11 @@ echo password | sudo -S make install
 clear
 
 echo "Compiling tests..."
-make
-make test
+sudo make
+sudo make test
 clear
 
 echo "Navigating to test..."
 cd examples
 cd SingleDIO
-./SingleDIO
+sudo ./SingleDIO
