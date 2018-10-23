1743ff5afd5ee206ce2e275572769077d9e6c4c6
rsenegor-bpc <44207137+rsenegor-bpc@users.noreply.github.com>
2018-10-23 14:27:36 -0700

    Update DIOtest.sh

--- DIOtest.sh
+++ DIOtest.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
+
 echo "Installing updates..."
 sudo apt-get update
-
-sudo apt-get install -y git-all
 
 sudo adduser mtsnlinux --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
 echo password | sudo chpasswd
 
 echo "Installing DIO test software..."
 sudo git clone https://github.com/rsenegor-bpc/finalDIO.git /home/mtsnlinux/Desktop
-sudo git clone https://github.com/rsenegor-bpc/DIOtest.sh.git /home/mtsnlinux/DIO
 
 cd /home/mtsnlinux/Desktop
 cd powerdaq-3.6.26
